ef7d0c58cd1a779df5c073f446b5a4bc5b842804
Anshul Shah <anshul.shah@mail.utoronto.ca>
2020-02-14 22:36:13 -0500

    Update app.py
    Setup basics of redirect

--- app.py
+++ app.py
@@ -4,9 +4,20 @@ app=Flask(__name__)
 
 @app.route('/')
 def root():
-    # tells Flask to render the HTML page called index.html
+    # tells Flask to render the HTML page called Home.html
     return render_template('Home.html')
 
+@app.route('/About_us')
+	return render_template('About_us.html')
+
+@app.route('/Home')
+	return render_template('Home.html')
+
+@app.route('/Login')
+	return render_template('Login.html')
+
+@app.route('/Admin_login')
+	return render_template('Admin_login.html')
 
 if __name__ == '__main__':
-	app.run(host= '0.0.0.0', port=9000)
+	app.run(debug=True, host= '0.0.0.0', port=9000)
