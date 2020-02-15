7fe5533e770ec21e3db32b6c8c24eb3584030b57
AbChatt <abhi1506@live.com>
2020-02-15 10:51:54 -0500

    Alpha test of facial recog api implementation

--- ML Code/opencv_facialrecog.py
+++ ML Code/opencv_facialrecog.py
@@ -2,33 +2,43 @@ import numpy as np
 import cv2
 import pickle
 import requests
+import face_recognition
 
 # refer to opencv_cameratest for detailed explanation of basic underlying code
 # note that we need to copy the data folder to the same directory as our code in a folder called cascades
 # experiment with different haar cascade models to see how we can grab facial features differently
 
-recognizer = cv2.face.LBPHFaceRecognizer_create()       # can use a different recogniser - such as a deep learning model
+#recognizer = cv2.face.LBPHFaceRecognizer_create()       # can use a different recogniser - such as a deep learning model
 face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')  # calling the appropriate face model
 #eye_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_eye.xml')    # calling the appropriate eye model
 #smile_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_smile.xml')    # calling the appropriate smile model
 
-recognizer.read("trainer.yml")                          # read from training model
+#recognizer.read("trainer.yml")                          # read from training model
 
-labels = {}
-with open("labels.pickle", 'rb') as f:                  # reading label ids from file as bytes
-    og_labels = pickle.load(f)
-    labels = {v:k for k, v in og_labels.items()}          # inverting key value relationship of dictionary - so we can get name
+#labels = {}
+#with open("labels.pickle", 'rb') as f:                  # reading label ids from file as bytes
+#    og_labels = pickle.load(f)
+#    labels = {v:k for k, v in og_labels.items()}          # inverting key value relationship of dictionary - so we can get name
 
-#cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)
 
-url = "http://100.80.129.11:8080/shot.jpg"          # specific to IP address, port 8080
+#url = "http://100.80.129.11:8080/shot.jpg"          # specific to IP address, port 8080
+
+encoding_lst = []
+label_lst = []
+
+known_image1 = face_recognition.load_image_file("image1.jpg")
+known_encoding1 = face_recognition.face_encodings(known_image1)[0]
+
+encoding_lst.append(known_encoding1)
+label_lst.append("Boss")
 
 while True:
-    #ret, frame = cap.read()
+    ret, frame = cap.read()
 
-    img_resp = requests.get(url)                    # stores the image at the URL
-    img_arr = np.array(bytearray(img_resp.content), dtype = np.uint8)   # converting image to numpy array
-    frame = cv2.imdecode(img_arr, -1)               # decoding numpy array into a picture that is readable
+    #img_resp = requests.get(url)                    # stores the image at the URL
+    #img_arr = np.array(bytearray(img_resp.content), dtype = np.uint8)   # converting image to numpy array
+    #frame = cv2.imdecode(img_arr, -1)               # decoding numpy array into a picture that is readable
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # Model must be trained on grayscale version of video feed (documentation guidelines)
 
@@ -38,25 +48,34 @@ while True:
     # h is bottomost coordinate of region of interest (height of roi), w is rightmost coordinate of region of interest (width of roi)
     for (x, y, w, h) in faces:                  # prints all the values from face (region of interest) in frame
         #print(x, y, w, h)
-        roi_gray = gray[y:y + h, x:x + w]       # region of interest for gray frame
+        #roi_gray = gray[y:y + h, x:x + w]       # region of interest for gray frame
         roi_color = frame[y:y + h, x:x + w]     # region of interest for colour frame. Same region as colour frame (of course)
 
         # Recognising the face. Perfect place to integrate a deep learning model such as keras / tensorflow / pytor / scikit learn
         # The approach below is not foolproof, but does work and is simple. Integrating deep learning models will complicate things
 
+        # recog API stuff
+
+        face_locations = face_recognition.face_locations(frame)
+
+        unknown_encoding = face_recognition.face_encodings(frame)[0]
+
+        results = face_recognition.compare_faces([known_encoding1], unknown_encoding)
+
         # need a lot of images and data to make this model somewhat good
 
-        id_, conf = recognizer.predict(roi_gray)    # predicting the region of interest (giving us the label and confidence of pred)
+        #id_, conf = recognizer.predict(roi_gray)    # predicting the region of interest (giving us the label and confidence of pred)
 
         # problem with approach is confidence varies a lot and goes above 100, so is not actually measuring confidence
-        if conf >= 45 and conf <= 85:
-            print(id_)                          # prints out training label
-            print(labels[id_])                  # prints out name associated with training label
+        #if conf >= 45 and conf <= 85:
+         #   print(id_)                          # prints out training label
+          #  print(labels[id_])                  # prints out name associated with training label
 
+        if results == True:
             # manually adding label to training image
             font = cv2.FONT_HERSHEY_SIMPLEX
-            name = labels[id_]
-            color = (255, 255, 255)     # white
+            name = label_lst[0]
+            color = (255, 0, 0)     # blue
             stroke = 2
 
             # attributes: source frame, name of label, coordinates, font, font size, color of label, thickness of label stroke
@@ -65,7 +84,7 @@ while True:
         # possible issues: image sizes, not real representation, wrong conf interval
         
         img_item = "my_image.png"               
-        cv2.imwrite(img_item, roi_gray)         # saves region of interest from gray frame to file
+        cv2.imwrite(img_item, roi_color)         # saves region of interest from color frame to file
 
         # draws rectangle from (x, y) to (x + w, y + h) of colour 'color', width 'stroke' around region of interest
         color = (255, 0, 0)                     # BGR value - blue colour. You can pick whatever you want of course
@@ -90,6 +109,7 @@ while True:
 
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
-
-cap.release()
-cv2.destroyAllWindows()
+    
+    cap.release()
+    cv2.destroyAllWindows()
+    
