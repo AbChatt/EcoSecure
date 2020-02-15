104389abc1481260614ca11e44ca28f1702a0802
soodshru <55465941+soodshru@users.noreply.github.com>
2020-02-15 05:56:58 -0500

    Updated app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, session, redirect, url_for, escape, request, render_tem
 app=Flask(__name__)
 
 @app.route('/')
+@app.route('/Home')
 def root():
     # tells Flask to render the HTML page called Home.html
     return render_template('Home.html')
@@ -10,10 +11,6 @@ def root():
 @app.route('/About_us')
 def About_us_page():
 	return render_template('About_us.html')
-
-@app.route('/Home')
-def Home_page():
-	return render_template('Home.html')
 
 @app.route('/Login')
 def Login_page():
