05cb76cdbcf539345b6eb82de41f81c263fc7c09
Anshul Shah <anshul.shah@mail.utoronto.ca>
2020-02-15 16:04:09 -0500

    Worked on Login & Signup Page

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
+import sqlite3
 from flask import Flask, session, redirect, url_for, escape, request, render_template, g
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
 
 app=Flask(__name__)
+app.secret_key = 'George#1'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///assignment3.db'
+db = SQLAlchemy(app)
 
 @app.route('/')
-@app.route('/Home')
 def root():
+	return render_template('index.html')
+
+@app.route('/Home')
+def Home_Page():
     # tells Flask to render the HTML page called Home.html
     return render_template('Home.html')
 
@@ -15,6 +24,16 @@ def About_us_page():
 @app.route('/Login')
 def Login_page():
 	return render_template('Login.html')
+
+@app.route('/signinform', methods=['GET', 'POST'])
+def signin_form():
+    if request.method == 'POST':
+    	LoginID = str(request.form['LoginID'])
+        Password = str(request.form['Password'])
+        if LoginID in session:
+            session['LoginID'] = LoginID
+            return render_template('Lecture.html')
+
 
 @app.route('/Admin_login')
 def Admin_login_page():
