cf7c1a6c1e1f1083e39d2426dfa693e8f965bdfc
demolix <singhsiddhant2013@gmail.com>
2020-02-15 20:36:31 -0500

    Update main_app.py

--- ML Code/main_app.py
+++ ML Code/main_app.py
@@ -22,17 +22,17 @@ obama_image = face_recognition.load_image_file("image2.jpg")
 obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
 
 # Load a second sample picture and learn how to recognize it.
-biden_image = face_recognition.load_image_file("biden.jpg")
-biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
+# biden_image = face_recognition.load_image_file("biden.jpg")
+# biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
 
 # Create arrays of known face encodings and their names
 known_face_encodings = [
     obama_face_encoding,
-    biden_face_encoding
+#    biden_face_encoding
 ]
 known_face_names = [
     "Sid",
-    "Lydia"
+#    "Lydia"
 ]
 
 # Initialize some variables
@@ -73,6 +73,7 @@ while True:
             if True in matches:
                 first_match_index = matches.index(True)
                 name = known_face_names[first_match_index]
+                
 
             # Or instead, use the known face with the smallest distance to the new face
             # face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
